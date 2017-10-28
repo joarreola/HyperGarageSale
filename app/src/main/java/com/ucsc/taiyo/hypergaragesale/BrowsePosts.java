c8d6d062e743f8e57e3303020fe4fbd654eaf426
taiyo <taiyo@taiyos-MacBook-Pro.local>
2017-10-28 11:57:15 -0700

    Pre-Advanced class commit.

---
+++ app/src/main/java/com/ucsc/taiyo/hypergaragesale/BrowsePosts.java
@@ -0,0 +1,15 @@
+package com.ucsc.taiyo.hypergaragesale;
+
+/**
+ * Created by taiyo on 6/5/17.
+ */
+
+public class BrowsePosts {
+    public String mTitle;
+    public String mPrice;
+
+    public BrowsePosts (String title, String price) {
+        this.mTitle = title;
+        this.mPrice = price;
+    }
+}
---
+++ app/src/main/java/com/ucsc/taiyo/hypergaragesale/Posts.java
@@ -0,0 +1,23 @@
+package com.ucsc.taiyo.hypergaragesale;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by taiyo on 6/5/17.
+ */
+
+public class Posts {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public Posts() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class PostEntry implements BaseColumns {
+        public static final String TABLE_NAME = "posts";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DESCRIPTION = "decription";
+        public static final String COLUMN_NAME_PRICE = "price";
+
+    }
+
+}
