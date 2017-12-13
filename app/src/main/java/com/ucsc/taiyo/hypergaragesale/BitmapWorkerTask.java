37ec14d17c74e775ba1c1e36d60ece3d75245e78
joarreola <taiyo472004@yahoo.com>
2017-12-13 11:08:10 -0800

    Code cleanup.

--- app/src/main/java/com/ucsc/taiyo/hypergaragesale/BitmapWorkerTask.java
+++ app/src/main/java/com/ucsc/taiyo/hypergaragesale/BitmapWorkerTask.java
@@ -3,7 +3,6 @@ package com.ucsc.taiyo.hypergaragesale;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ImageView;
-
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,7 +11,6 @@ import java.io.OutputStream;
 import java.lang.ref.WeakReference;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
 import android.os.AsyncTask;
 import com.jakewharton.disklrucache.*;
 
@@ -26,34 +24,39 @@ public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
     private static final int DISK_CACHE_INDEX = 0;
     private static final String TAG = "ImageCache";
     private final Object mDiskCacheLock = new Object();
+
+    // TODO: where set to false?
     private boolean mDiskCacheStarting = true;
 
+
     public BitmapWorkerTask(ImageView imageView, int reqHeight, int reqWidth) {
+
         // Use a WeakReference to ensure the ImageView can be garbage collected
         imageViewReference = new WeakReference(imageView);
+
         int reqWidth1 = reqWidth;
+
         this.reqHeight = reqHeight;
     }
 
-    // Decode image in background.
     @Override
     protected Bitmap doInBackground(String... params) {
+
         String file = params[0];
 
-
-        // Check disk cache in background thread
         Bitmap bitmap = getBitmapFromDiskCache(file);
 
-        if (bitmap == null) { // Not found in disk cache
+        if (bitmap == null) {
 
             bitmap =
                     new BitmapFactoryUtilities().decodeSampledBitmapFromFile(file,
                             reqHeight, reqHeight);
         }
 
-        // Add final bitmap to caches
         try {
+
             addBitmapToCache(file, bitmap);
+
         } catch (Exception e)
         {
             Log.e("addBitmapToCache", e.getMessage());
@@ -62,12 +65,14 @@ public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
         return bitmap;
     }
 
-    // Once complete, see if ImageView is still around and set bitmap.
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         if (imageViewReference != null && bitmap != null) {
+
             final ImageView imageView = (ImageView) imageViewReference.get();
+
             if (imageView != null) {
+
                 imageView.setImageBitmap(bitmap);
             }
         }
@@ -84,47 +89,67 @@ public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
 
     /**
      * Adds a bitmap to both memory and disk cache. (From BitmapFun)
+     *
      * @param data Unique identifier for the bitmap to store
      * @param bitmap The bitmap drawable to store
      */
     public synchronized void addBitmapToCache(String data, Bitmap bitmap) {
+
         if (data == null || bitmap == null) {
+
             return;
         }
 
-        // Add to memory cache as before
         if (getBitmapFromMemCache(data) == null) {
+
             BrowsePostsActivity.mMemoryCache.put(data, bitmap);
         }
 
         synchronized (mDiskCacheLock) {
+
             // Add to disk cache
             if (BrowsePostsActivity.mDiskCache != null) {
+
                 final String key = hashKeyForDisk(data);
+
                 OutputStream out = null;
+
                 try {
                     DiskLruCache.Snapshot snapshot = BrowsePostsActivity.mDiskCache.get(key);
+
                     if (snapshot == null) {
+
                         final DiskLruCache.Editor editor = BrowsePostsActivity.mDiskCache.edit(key);
+
                         if (editor != null) {
                             out = editor.newOutputStream(DISK_CACHE_INDEX);
-                            //value.getBitmap().compress(
-                            //        mCacheParams.compressFormat, mCacheParams.compressQuality, out);
+
                             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
                             editor.commit();
+
                             out.close();
                         }
-                    } else {
+
+                    }
+                    else {
+
                         snapshot.getInputStream(DISK_CACHE_INDEX).close();
                     }
-                } catch (Exception e) {
-                    Log.e(TAG, "addBitmapToCache - " + e);
-                } finally {
+                }
+                catch (Exception e) {
+
+                    Log.e(TAG, "addBitmapToDiskCache - commit" + e);
+                }
+                finally {
                     try {
                         if (out != null) {
                             out.close();
                         }
-                    } catch (IOException e) {}
+                    } catch (IOException e) {
+
+                        Log.e(TAG, "addBitmapToDiskCache - close" + e);
+                    }
                 }
             }
         }
@@ -137,12 +162,13 @@ public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
      * @return The bitmap if found in cache, null otherwise
      */
     public Bitmap getBitmapFromDiskCache(String data) {
+
         final String key = hashKeyForDisk(data);
+
         Bitmap bitmap = null;
 
         synchronized (mDiskCacheLock) {
-            // getting stuck here.
-            // not anymore, but no images in BrowserPostsActivity (?)
+            // TODO: mDiskCacheStarting never set to false, thus timeout! - fix
             /*
             while (mDiskCacheStarting) {
                 try {
@@ -152,37 +178,49 @@ public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
             */
 
             if (BrowsePostsActivity.mDiskCache != null) {
+
                 InputStream inputStream = null;
+
                 try {
+
                     final DiskLruCache.Snapshot snapshot = BrowsePostsActivity.mDiskCache.get(key);
+
                     if (snapshot != null) {
-                        if (BuildConfig.DEBUG) {
-                            Log.d(TAG, "Disk cache hit");
-                        }
+
                         inputStream = snapshot.getInputStream(DISK_CACHE_INDEX);
+
                         if (inputStream != null) {
+
                             FileDescriptor fd = ((FileInputStream) inputStream).getFD();
 
-                            // Decode bitmap, but we don't want to sample so give
-                            // MAX_VALUE as the target dimensions
-                            //bitmap = DiskLruCache.ImageResizer.decodeSampledBitmapFromDescriptor(
-                            //        fd, Integer.MAX_VALUE, Integer.MAX_VALUE, this);
-
-                            //bitmap =  new BitmapFactoryUtilities().decodeSampledBitmapFromInputStream(inputStream,  500, 500);
-                            bitmap =  new BitmapFactoryUtilities().decodeSampledBitmapFromDescriptor(fd,  500, 500);
+                            bitmap =
+                                    new BitmapFactoryUtilities().decodeSampledBitmapFromDescriptor(fd,
+                                            500, 500);
 
                         }
                     }
-                } catch (final IOException e) {
-                    Log.e(TAG, "getBitmapFromDiskCache - " + e);
-                } finally {
+                }
+                catch (final IOException e) {
+
+                    Log.e(TAG, "getBitmapFromDiskCache - get bitmap" + e);
+
+                }
+                finally {
                     try {
+
                         if (inputStream != null) {
+
                             inputStream.close();
                         }
-                    } catch (IOException e) {}
+
+                    }
+                    catch (IOException e) {
+
+                        Log.e(TAG, "getBitmapFromDiskCache - inputStream.close" + e);
+                    }
                 }
             }
+
             return bitmap;
         }
     }
@@ -196,28 +234,46 @@ public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
     /**
      * A hashing method that changes a string (like a URL) into a hash suitable
      * for using as a disk filename. (From BitmapFun)
+     *
+     * @param key
+     * @return
      */
     public static String hashKeyForDisk(String key) {
+
         String cacheKey;
+
         try {
+
             final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+
             mDigest.update(key.getBytes());
+
             cacheKey = bytesToHexString(mDigest.digest());
-        } catch (NoSuchAlgorithmException e) {
+        }
+        catch (NoSuchAlgorithmException e) {
+
             cacheKey = String.valueOf(key.hashCode());
         }
+
         return cacheKey;
     }
 
     private static String bytesToHexString(byte[] bytes) {
+
         StringBuilder sb = new StringBuilder();
+
         for (byte aByte : bytes) {
+
             String hex = Integer.toHexString(0xFF & aByte);
+
             if (hex.length() == 1) {
+
                 sb.append('0');
             }
+
             sb.append(hex);
         }
+
         return sb.toString();
     }
 
